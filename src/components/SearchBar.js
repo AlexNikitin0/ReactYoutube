71175ae0813099c4f16166c455a537e90fb0f945
Alex <alex.nikitin15@yahoo.com>
2020-01-13 14:46:44 -0500

    First commit. Created boilerplate components with youtube API

---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class SearchBar extends React.Component {
+    state = {
+        searchTerm: '',
+    }
+    render() {
+        return(
+            <h1>searchbar</h1>
+        )
+    }
+}
+
+export default SearchBar;
---
+++ src/components/VideoDetails.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const VideoDetails = () => {
+    return (
+        <h1>this is video details component</h1>
+    )
+}
+
+export default VideoDetails
+
